1e98c22b0ffb8411bb37317a392bff3fd624014b
Letícia Coutinho da Silva <leticianejor@gmail.com>
2021-07-14 15:36:08 -0300

    Initial commit

---
+++ Condicional_If/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Condicional_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string nomeC = "Leticia";
+            const string cabeloC = "Morena";
+            const int idadeC = 19;
+
+            Console.WriteLine("Informe o nome da professora");
+            string nomeProf = Console.ReadLine();
+
+
+            Console.WriteLine("Informe a idade dela");
+            int idadeProf = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a cor do cabelo dela");
+            string cabeloProf = Console.ReadLine();
+
+            if (nomeC == nomeProf && cabeloC == cabeloProf && idadeC == idadeProf)
+                Console.WriteLine("É exatamente ela!");
+            else if (nomeC == nomeProf || cabeloC == cabeloProf || idadeC == idadeProf)
+                Console.WriteLine("Parece com ela, mas não é ela!");
+            else
+                Console.WriteLine("Não é ela!");
+
+            //string mensagem = nomeC == nomeProf && cabeloC == cabeloProf && idadeC == idadeProf ? "É exatamente ela!" :
+            //  nomeC == nomeProf || cabeloC == cabeloProf || idadeC == idadeProf ? "Parece com ela, mas não é ela!" : "Não é ela!";
+        }
+    }
+}
---
+++ Condicional_If_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Condicional_If_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(mensagem());
+            Console.WriteLine("\nEscreva seu nome:");
+
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("\nInforme a sua idade:");
+
+            if (int.TryParse(Console.ReadLine(), out int idadeConvertida))
+                Console.WriteLine("\nIdade inválida");
+            else
+            {
+                int idade = idadeConvertida;
+                //int idade = int.Parse(Console.ReadLine());
+
+                // Operador Ternário
+                string eMaior = idade >= 18 ? "maior" : "menor";
+
+                Console.WriteLine($"\n{nome}, você é {eMaior} de idade!");
+                Console.WriteLine($"\nParte 2 - {nome}, você é {(idade >= 18 ? "maior" : "menor")} de idade");
+            }
+        }
+
+        private static string mensagem()
+        {
+            return "OIIII";
+        }
+    }
+}
